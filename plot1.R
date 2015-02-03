ecee83d5bf7d60f034cdfab648e913890a5395b6
Hrafnkell Eiríksson <he@klaki.net>
2015-02-03 18:18:00 +0000

    Adding scripts and plots

---
+++ plot1.R
@@ -0,0 +1,20 @@
+library(lubridate)
+library(dplyr)
+# set the locale for time to english so we can get x axis day names in english
+Sys.setlocale("LC_TIME", 'English')
+
+if(!file.exists("household_power_consumption.txt"))
+{
+  download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",dest="household_power_consumption.zip")
+  unzip("household_power_consumption.zip")
+}
+
+# we read only the column headings first and then only the lines corresponding to the wanted dates 
+pow_head<-read.table('household_power_consumption.txt', stringsAsFactors=F, sep=";", na.strings=c("?"),header=TRUE,nrows=1)
+pow<-read.table('household_power_consumption.txt', stringsAsFactors=F, sep=";", na.strings=c("?"),skip=66637,nrows=2880,col.names=colnames(pow_head))
+pow<-mutate(pow,Date2=dmy_hms(paste(Date,Time)))
+
+png(filename="plot1.png",width=480, height=480)
+hist(pow$Global_active_power, xlab="Global Active Power (kilowatts)",xaxt='n',col="red",main="Global Active Power",ylim=c(0,1200))
+axis(side=1, at=seq(0,7,2), labels=seq(0,7,2),xlim=c(0,10))
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,23 @@
+library(lubridate)
+library(dplyr)
+# set the locale for time to english so we can get x axis day names in english
+Sys.setlocale("LC_TIME", 'English')
+
+if(!file.exists("household_power_consumption.txt"))
+{
+  download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",dest="household_power_consumption.zip")
+  unzip("household_power_consumption.zip")
+}
+
+# we read only the column headings first and then only the lines corresponding to the wanted dates 
+pow_head<-read.table('household_power_consumption.txt', stringsAsFactors=F, sep=";", na.strings=c("?"),header=TRUE,nrows=1)
+pow<-read.table('household_power_consumption.txt', stringsAsFactors=F, sep=";", na.strings=c("?"),skip=66637,nrows=2880,col.names=colnames(pow_head))
+pow<-mutate(pow,Date2=dmy_hms(paste(Date,Time)))
+
+
+png(filename="plot3.png",width=480, height=480)
+plot(pow$Sub_metering_1 ~ pow$Date2,type="l", ylab="Energy sub metering",xlab="")
+points(pow$Sub_metering_2 ~ pow$Date2,type="l",col="red")
+points(pow$Sub_metering_3 ~ pow$Date2,type="l",col="blue")
+legend("topright", lty=c(1,1),col = c("black","blue", "red"), legend = c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"))
+dev.off()
